bdeaf32fa58d1c470a452cff60213dbc3057055c
Danny van Swieten <danny@resolume.com>
2023-04-29 17:19:49 +0200

    clean

--- examples/material_compiler.rs
+++ examples/material_compiler.rs
@@ -7,13 +7,7 @@ pub fn main() {
     let random_module = std::env::current_dir()
         .unwrap()
         .join("./example_shaders/ray_gen.glsl");
-    // let mut material_compiler = MaterialCompiler::new();
-    // material_compiler.add_shader(Box::new(DiffuseShader::new()));
-    // material_compiler.add_shader(Box::new(MirrorShader::new()));
-    // material_compiler.include(&random_module);
-    // let src = material_compiler.compile();
-    // println!("{}", src);
 
-    let material_compiler = ShaderCompiler::from_path(&random_module);
-    println!("{}", material_compiler.compile());
+    let shader_compiler = ShaderCompiler::from_path(&random_module);
+    println!("{}", shader_compiler.compile());
 }
