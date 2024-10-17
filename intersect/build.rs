cc0f5c27094323e3ac2c05b64d42b4c2fb5bc383
Danny van Swieten <danny@resolume.com>
2024-10-17 16:40:07 +0200

    only run build script on macos

--- intersect/build.rs
+++ intersect/build.rs
@@ -2,18 +2,21 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let vulkan_sdk = env::current_dir()
-        .unwrap()
-        .join("../../../../../../VulkanSDK/MacOS/lib");
+    #[cfg(target_os = "macos")]
+    {
+        let vulkan_sdk = env::current_dir()
+            .unwrap()
+            .join("../../../../../../VulkanSDK/MacOS/lib");
 
-    let cmd = "@executable_path/".to_string() + vulkan_sdk.to_str().unwrap();
+        let cmd = "@executable_path/".to_string() + vulkan_sdk.to_str().unwrap();
 
-    Command::new("install_name_tool")
-        .arg("-add_rpath")
-        .arg(cmd)
-        .arg("intersect/target/debug/examples/gpu")
-        .status()
-        .expect("Failed to add rpath to gpu example");
+        Command::new("install_name_tool")
+            .arg("-add_rpath")
+            .arg(cmd)
+            .arg("intersect/target/debug/examples/gpu")
+            .status()
+            .expect("Failed to add rpath to gpu example");
 
-    println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed=build.rs");
+    }
 }
